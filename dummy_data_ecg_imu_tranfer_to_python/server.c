706624f234b83b44d1927bd7fbe502ee4899a71f
Maliya11 <114799896+Maliya11@users.noreply.github.com>
2024-04-30 14:45:32 +0530

    editing7

--- dummy_data_ecg_imu_tranfer_to_python/server.c
+++ dummy_data_ecg_imu_tranfer_to_python/server.c
@@ -61,7 +61,7 @@ void *read_main_tcp (void *arg)
 	while(rx_count>0)
 	{
 //		main_tcp_message_parse(conn,rx_buffer,rx_count);
-		rx_count = read(conn->tcp_socket,rx_buffer,MAX_TCP_PACKET_SIZE);
+		rx_count = read(tcp_socket,rx_buffer,MAX_TCP_PACKET_SIZE); //conn->
 	}
 
 
