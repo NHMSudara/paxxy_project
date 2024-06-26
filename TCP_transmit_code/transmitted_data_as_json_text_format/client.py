ce7263607e665642a5c8fde7a508266990b53582
K-Vithusan <vithusankennedy@gmail.com>
2024-06-26 10:01:14 +0530

    edits to access 3000 samples in client.py

--- TCP_transmit_code/transmitted_data_as_json_text_format/client.py
+++ TCP_transmit_code/transmitted_data_as_json_text_format/client.py
@@ -4,44 +4,32 @@ import subprocess
 
 from intermediate import Sensor_Data
 
-ECG_Array = []
-
-
-
 def main():
 
-    subprocess.Popen(["python3", "intermediate.py"])  
+
  
-    E_queue1 = multiprocessing.Queue(maxsize=3500)
+    queue = multiprocessing.Queue()
 
+    Data_Process = Sensor_Data(queue)
 
-
-
-    ecg_batch = 0
-    
-
-    Data_Process = Sensor_Data(E_queue1)
     process = multiprocessing.Process(target=Data_Process.Process_Data)
 
     process.start()
 
     try:
         while True:
-            ECG_Array.append(E_queue1.get())
-            if len(ECG_Array) >= 3000:
-                
-                print(f'ECG batch : {ECG_Array[0]["ecg"]}')
-                # for i, E_row in enumerate(ECG_Array):
-                #     print(f'{i} :   {E_row}')
-                E_queue1.empty()
-                ECG_Array.clear()
-                
+            if not queue.empty():
+                Array = queue.get(0:3)
+                print(Array)
+                print(type(Array))
+                print(f'successfully Printed !!!!!!!!!!!')
+
+
+            
                 
     except KeyboardInterrupt:
         pass
     
-
- 
 
     
 if __name__ == "__main__":
